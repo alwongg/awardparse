552b25a253a317cbce6f9bea8fef543dad43e25d
Alex Wong <wong_1994@hotmail.com>
2024-12-22 01:02:46 -0500

    Add Installer

--- options.py
+++ options.py
@@ -9,18 +9,19 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description='Options for ResumeCLT')
 
-    parser.add_argument('--source_dir', type=str, required=True,
+    parser.add_argument('--source_dir', type=str, required=False, default="test_resume",
                         help='Directory where the resume files are stored')
-    parser.add_argument('--output_dir', type=str, required=True,
+    parser.add_argument('--output_dir', type=str, required=False, default="output",
                         help='Directory where the output files will be stored')
-    parser.add_argument('--target_list', type=str, required=False,
+    parser.add_argument('--target_list', type=str, required=False, default="test_school_list.txt",
                         help='File containing the list of target schools')
-    parser.add_argument('--award_list', type=str, required=False,
+    parser.add_argument('--award_list', type=str, required=False, default="award_list.txt",
                         help='Path to the award titles list file.')
-    parser.add_argument('--award_list2', type=str, required=False,
+    parser.add_argument('--award_list2', type=str, required=False, default="award_list2.txt",
                         help='Path to the award titles list file.')
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = parse_args()
